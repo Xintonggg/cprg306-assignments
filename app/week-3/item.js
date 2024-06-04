17faa726c5909cee37c09e1cc7ed1cffc8ffd40f
Xintonggg <141611314+Xintonggg@users.noreply.github.com>
2024-06-04 00:58:38 -0600

    Beauty Magic
    Re-edited the font color and background color to make it look better ：）

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,13 +1,13 @@
 import ItemList from "./item-list"
 
 
-export default function Item({name, quantity, category}){
+export default function Item({name, quantity, category,}){
 
     
     return(
-        <div className="text-white bg-pink-400 border border-cyan-100 m-10 p-10">
-            <h3 className="text_lg">{name}</h3>
-            <p>Buy {quantity} in {category}</p>
+        <div className="text-white bg-pink-300 border border-yellow-400 m-10 p-5 w-96">
+            <h3 className="text-2xl font-bold">{name}</h3>
+            <p className="text-lg">Buy {quantity} in {category}</p>
         </div>
            
     )
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,8 +3,8 @@ import ItemList from './item-list';
 
 export default function Page(){
     return (
-        <main>
-            <h1>Shopping List</h1>
+        <main className='bg-yellow-100'>
+            <h1 className="text-yellow-300 text-3xl font-bold mb-4" >Shopping List</h1>
             <ItemList/>
             
 
