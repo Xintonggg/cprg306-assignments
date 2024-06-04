f4dd0d02fc3979308105aead4554fcb3eae9c273
Xintonggg <141611314+Xintonggg@users.noreply.github.com>
2024-06-04 01:17:39 -0600

    edit the main page

--- app/page.js
+++ app/page.js
@@ -3,13 +3,16 @@ import Link from "next/link";
 
 export default function Home(){
   return(
-    <main>
-      <h1>CPRG306: Web Development 2 - Assignments</h1>
-      <li>
+    <main className="bg-yellow-100 ">
+      <h1 className="text-pink-300 text-3xl text-center font-bold">CPRG306: Web Development 2 - Assignments</h1>
+      <li className="text-yellow-300 text-xl font-bold">
         <Link href={"./week-2/"}>Week 2 Assignment</Link>
       </li>
-      <li>
+      <li className="text-yellow-300 text-xl font-bold">
         <Link href={"./week-3/"}>Week 3 Assignment</Link>
+      </li>
+      <li className="text-yellow-300 text-xl font-bold">
+        <Link href={"./week-4/"}>Week 4 Assignment</Link>
       </li>
 
       
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ItemList from './item-list';
+import Link from 'next/link';
 
 export default function Page(){
     return (
-        <main className='bg-yellow-100'>
+        <main className='bg-yellow-100 h-25'>
             <h1 className="text-yellow-300 text-3xl font-bold mb-4" >Shopping List</h1>
             <ItemList/>
-            
+            <p className='text-yellow-300 text-2xl font-bold '>
+                <Link href={"../"}>Back</Link>
+            </p>
 
            
         </main>
