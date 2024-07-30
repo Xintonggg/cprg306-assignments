1df10499ba0c01c4a17a5637f2144ca41de1a91d
Xintonggg <141611314+Xintonggg@users.noreply.github.com>
2024-07-30 02:27:53 -0600

    Why I cannot push my code？？？

---
+++ app/week-7/new-item.js
@@ -0,0 +1,72 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function NewItem({ onAddItem }) {
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  const [category, setCategory] = useState('produce');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = { name, quantity, category };
+    onAddItem(newItem);
+    alert(`Item: ${name}\nQuantity: ${quantity}\nCategory: ${category}`);
+    setName('');
+    setQuantity(1);
+    setCategory('produce');
+  };
+
+  return ( 
+    <form onSubmit={handleSubmit} className="p-8 space-y-4 bg-yellow-700 rounded-lg shadow-lg w-80 mx-5 my-5">
+      <div>
+        <input
+          type="text"
+          placeholder="Item name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          className="w-full p-2 mb-2 text-yellow-700 placeholder-gray-400 bg-yellow-100 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      <div className="flex space-x-2">
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+          min="1"
+          max="100"
+          required
+          className="w-1/3 p-2 text-yellow-700 placeholder-gray-400 bg-yellow-100 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+          className="w-2/3 p-2 text-yellow-700 bg-yellow-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          <option value="" disabled>Category</option>
+          <option value="produce">Produce</option>
+          <option value="dairy">Dairy</option>
+          <option value="bakery">Bakery</option>
+          <option value="meat">Meat</option>
+          <option value="frozen-foods">Frozen Foods</option>
+          <option value="canned-goods">Canned Goods</option>
+          <option value="dry-goods">Dry Goods</option>
+          <option value="beverages">Beverages</option>
+          <option value="snacks">Snacks</option>
+          <option value="household">Household</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <button
+        type="submit"
+        className="w-full p-2 text-yellow-700 bg-yellow-300 rounded-md hover:bg-yellow-500 active:bg-pink-400"
+      >
+        +
+      </button>
+    </form>
+  );
+}
