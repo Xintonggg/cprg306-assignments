6ecd988a4a55fee72748d99fdb32f39fce77513e
Xintonggg <141611314+Xintonggg@users.noreply.github.com>
2024-08-08 23:04:22 -0600

    fix page color

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -23,24 +23,25 @@ export default function SignInPage() {
   }
 
   return (
-    <main className='h-screen flex justify-center items-center'>
+    <main className='h-screen flex justify-center items-center bg-yellow-100'>
 
       {user ? (
         // user IS logged in
 
 
-        <div className='h-96 w-96 shadow-lg p-8 bg-green-50 flex flex-col items-center rounded-lg'>
+        <div className='h-96 w-96 shadow-lg p-8 bg-yellow-200 flex flex-col items-center rounded-lg'>
           <div className='flex flex-col items-center mb-10'>
               <p className='text-2xl font-bold'>Welcome {user.displayName}</p>
               <img className="w-20 h-20 mt-10" src={user.photoURL} />
           </div>
 
           <div className="w-full my-5 flex justify-around "> 
-            <button className="bg-green-300 text-white px-4 py-2 rounded w-auto h-10 mb-2 mx-5"> 
-              <Link href={"./shopping-list/"}>HomePage </Link>
+            <button className="bg-yellow-800 text-white px-4 py-2 rounded w-auto h-10 mb-2 mx-5"> 
+              <Link href={"./logIn/shopping-list/"}>Shopping list </Link>
             </button>
-            <button onClick={handleSignOut} className="bg-white text-black px-4 py-2 rounded w-auto h-10 mx-5 hover:bg-green-300">Sign Out</button>
+            <button onClick={handleSignOut} className="bg-white text-black px-4 py-2 rounded w-auto h-10 mx-5 hover:bg-yellow-400">Sign Out</button>
           </div>
+         
         </div>
       ) : (
 
@@ -48,18 +49,15 @@ export default function SignInPage() {
 
         
         // user IS NOT logged in
-        <div className='h-96 w-96 shadow-lg p-8 bg-green-50 flex flex-col items-center rounded-lg'>
-          <div className="flex items-center mb-10">
-            <img src="/logo.jpg" alt="Description of the image" className="mr-2 h-10" />
-            <h1 className=' text-2xl font-bold'>BOOKIE</h1>
-           
+        <div className='h-96 w-96 shadow-lg p-8  bg-yellow-200 flex flex-col items-center rounded-lg'>
+          <div className="flex items-center mb-10">  
           </div>
           
 
           <h1 className='my-4 text-xl'>Log in by Github</h1>
           <button
             onClick={handleSignIn}
-            className="text-lg mt-4 hover:underline bg-green-200 px-4 py-2 rounded "
+            className="text-lg mt-4 hover:underline bg-yellow-800 px-4 py-2 rounded "
           >
             Sign In
           </button>
